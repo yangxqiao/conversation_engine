7d83f016c333c990004c79cc4d7eb22531a10255
Audrow Nash <anash103@gmail.com>
2019-01-30 20:25:11 -0800

    Add text expansion example

--- text_expansion.py
+++ text_expansion.py
@@ -1,13 +1,35 @@
 tags = {
-    "greeting": [
-        "Hi, {name}",
-    ],
-    "name": "Yang",
+    "greeting": "Hi, {name}.",
+    "name": "Yang-{nickname}",
+    "nickname": "smarty-pants",
 }
 
 def display(str):
-#    for items in str:
-#        print items
-    str.find()
+    s, e = find_curly_braces(str)
+    new_string = replace_in_braces(str, s, e)
+    print(new_string)
 
-display("{greeting}")
+    s, e = find_curly_braces(new_string)
+    new_string = replace_in_braces(new_string, s, e)
+    print(new_string)
+
+    s, e = find_curly_braces(new_string)
+    new_string = replace_in_braces(new_string, s, e)
+    print(new_string)
+
+def find_curly_braces(str):
+    start_idx = str.find("{")
+    end_idx = str.find("}")
+    return start_idx, end_idx
+
+def replace_in_braces(str, start_idx, end_idx):
+    substring = str[start_idx+1:end_idx]
+    try:
+        replacement = tags[substring]
+    except KeyError:
+        return None
+
+    new_string = str[0:start_idx] + replacement + str[end_idx+1:]
+    return new_string
+
+display("{greeting} How are you?")
