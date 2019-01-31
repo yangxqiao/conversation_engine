9a6b84b99c6bca6e90f5f65639ea16d125d2fceb
Xiaoyang Qiao <xiaoyanq@usc.edu>
2019-01-30 20:07:04 -0800

    Text

---
+++ text_expansion.py
@@ -0,0 +1,13 @@
+tags = {
+    "greeting": [
+        "Hi, {name}",
+    ],
+    "name": "Yang",
+}
+
+def display(str):
+#    for items in str:
+#        print items
+    str.find()
+
+display("{greeting}")
