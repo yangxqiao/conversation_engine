21509e3cc6b65c4353d7e4abef20da9225478560
Audrow Nash <anash103@gmail.com>
2019-02-02 16:45:07 -0800

    Add messages to exceptions

--- text_expansion.py
+++ text_expansion.py
@@ -1,14 +1,6 @@
-class Foo:
-    def __init__(self):
-        self.name = "foo"
-
-    def __repr__(self):
-        return self.name
-
-
 tags = {
     "greeting": "Hi, {name}, who's {age} years old.",
-    "name": "Yang-{nickname}}",
+    "name": "Yang-{nickname}",
     "nickname": "smarty-pants",
     "age": 18,
 }
@@ -18,21 +10,21 @@ def expand(string):
     while is_curly_braces_pair(string):
         s, e = find_curly_braces(string)
         string = replace_in_braces(string, s, e)
-        print string
+        print(string)
     return string
 
 
 def is_curly_braces_pair(string):
     start_idx = string.find("{")
     end_idx = string.find("}")
-    print start_idx, end_idx
+    print(start_idx, end_idx)
     if start_idx != -1 and end_idx != -1:
         if start_idx < end_idx:
             return True
         else:
-            raise IOError
+            raise IOError("Found a closed parentheses before and open one => check your input")
     elif start_idx != -1 or end_idx != -1:
-        raise IOError
+        raise IOError("Only found one parenthesis")
     else:
         return False
 
@@ -46,33 +38,17 @@ def find_curly_braces(string):
         raise IOError
 
 
-"""
-def find_curly_braces(str):
-    if is_curly_braces_pair(str):
-        start_idx = str.find('{')
-        imd = str[start_idx + 1:].find('{')
-        end_idx = 0
-        for i in range(start_idx, imd + start_idx):
-            if str[i] == '}':
-                end_idx = i
-        return start_idx, end_idx
-    else:
-        raise IOError
-"""
-
-
 def replace_in_braces(string, start_idx, end_idx):
     substring = string[start_idx+1:end_idx]
     try:
         replacement = str(tags[substring])
     except KeyError:
-        return None
+        raise KeyError("No match for key \'%s\'" % substring)
 
     new_string = string[0:start_idx] + replacement + string[end_idx+1:]
     return new_string
 
 try:
-    print expand("{greeting} How are you?")
-except IOError, e:
-    print e
-
+    print(expand("{greeting} How are you?"))
+except Exception as e:
+    print("EXCEPTION:", e)
